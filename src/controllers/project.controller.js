6abb00b2819358d1658a0e207f602ab52844b7f0
ASHOKJAAT12 <ajat39963@gmail.com>
2026-08-13 01:19:57 +0530

    project fetch by id and get project member functionlty add

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -69,8 +69,84 @@ const getProjects = asyncHandler(async (req, res) => {
 });
 
 
-const getProjectById = asyncHandler(async (req, res) => { });
-const getProjectMember = asyncHandler(async (req, res) => { });
+const getProjectById = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if ( ! project ) {
+        throw new ApiError(404,"Project not found.");
+    }
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,project,"Project fetched successfully.")
+    );
+ });
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
+
+    if ( !project ) {
+        throw new ApiError(404,"Project not found.")
+    }
+
+    const projectMembers = await ProjectMember.aggregate([
+        {
+            $match: {
+                project: new mongoose.Types.ObjectId(projectId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 1,
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                user: {
+                    $arrayElemAt: ["$user",0],
+                },
+            },
+        },
+        {
+            $project: {
+                project: 1,
+                user: 1,
+                role: 1,
+                createAt: 1,
+                updateAt: 1,
+                _id: 0,
+            },
+        },
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            projectMembers,
+            "Project member fetched successfully."
+        )
+    );
+ });
+ 
 const createProject = asyncHandler(async (req, res) => { });
 const updateProject = asyncHandler(async (req, res) => { });
 const deleteProject = asyncHandler(async (req, res) => { });
@@ -82,7 +158,7 @@ const updateMemberRole = asyncHandler(async (req, res) => { });
 export {
     getProjects,
     getProjectById,
-    getProjectMember,
+    getProjectMembers,
     createProject,
     updateProject,
     deleteProject,
